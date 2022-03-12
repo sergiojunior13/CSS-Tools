822a1653001268af0bc9b4981172c853dd7a3562
Sérgio Júnior <84044055+SergioJunior13@users.noreply.github.com>
2022-03-12 10:47:07 -0300

    Add files via upload

---
+++ defaultConfig.js
@@ -0,0 +1,54 @@
+const body = document.body
+const bodyElements = [...body.children]
+const header = document.createElement("header")
+header.translate = false
+header.innerHTML = `<h1><a href="../index.html">CSS Tools</a></h1>
+<nav id="nav">
+    <a href="../gradient-generator">Gradient</a>
+    <a href="../box-shadow-generator">Box-Shadow</a>
+    <a href="../filter-generator">Filter</a>
+    <a href="../border-radius-generator">Border-Radius</a>
+</nav>
+<img src="../images/menu-icon.svg" class="menu" id="menu">`
+
+body.innerHTML = ""
+body.appendChild(header)
+bodyElements.map(item => body.appendChild(item))
+
+function copy() {
+    navigator.clipboard.writeText(codeElement.textContent);
+    var div = document.createElement("div")
+    div.innerHTML = "Copied!"
+    div.classList = "copyDiv"
+    document.body.appendChild(div)
+    setTimeout(function(){
+        div.style.transform = "translateY(0px)"
+    }, 10)
+
+    setTimeout(function(){
+        div.style.transform = "translateY(60px)"
+       setTimeout(function(){
+        document.body.removeChild(div)
+       },1000)
+    }, 1300)
+}
+
+var menu = document.getElementById("menu")
+var nav = document.getElementById("nav")
+
+menu.addEventListener("click", mostrarMenu)
+
+function mostrarMenu() {
+    if(nav.style.display == ""){
+        nav.style.display = "flex"
+        setTimeout(() => {
+            nav.style.opacity = 1
+        }, 10);
+    }
+    else {
+        nav.style.opacity = 0
+        setTimeout(() => {
+            nav.style.display = ""
+        }, 500);
+    }
+}
--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+class Item {
+    constructor(name, href, img) {
+        this._name = name
+        this._href = href
+        this._img = img
+
+        var liElement = document.createElement("li")
+        liElement.innerHTML = `<a href="${this._href}">
+        <p style ="background-image: url('${this._img}');">${this._name}</p>
+    </a>`
+        return liElement
+    }
+}
+
+const listElement = document.querySelector("#list")
+listElement.append(new Item("Gradient Generator", "gradient-generator", "images/gradient-generator.jpeg"),
+    new Item("Box-Shadow Generator", "box-shadow-generator", "images/box-shadow.jpeg"),
+    new Item("Filter Generator", "filter-generator", "images/filter-generator.jpeg"),
+    new Item("Border-Radius Generator", "border-radius-generator", "images/border-radius.jpeg"))
+
 var menu = document.getElementById("menu")
 var nav = document.getElementById("nav")
 
 menu.addEventListener("click", mostrarMenu)
 
 function mostrarMenu() {
-    if(nav.style.display == ""){
+    if (nav.style.display == "") {
         nav.style.display = "flex"
         setTimeout(() => {
             nav.style.opacity = 1
