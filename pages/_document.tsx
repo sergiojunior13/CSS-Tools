4191d0644da5acbd776a38003693967aafebecba
Sergio Junior <sergiaraujo15@gmail.com>
2023-02-18 20:28:24 -0300

    change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,12 @@ export default function Document() {
     <Html lang="en-us" className="bg-zinc-800">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="shortcut icon" href="favicon.png" />{" "}
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="true"
         />
-
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-G0MQ749VYG"
@@ -31,19 +31,15 @@ export default function Document() {
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         />
-
         <meta
           name="google-site-verification"
           content="AqCTozOdxdev-fEuiuWVG5DrgFKrUb3b_BIH5nbyYE8"
         />
-
         <meta
           name="description"
           content="Online CSS Tools to help you make your website"
         />
-
         <meta name="keywords" content="css tools, css, web tools" />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
           rel="stylesheet"
