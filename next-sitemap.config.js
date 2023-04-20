c88a4778e63989a485fa1f8ce0b04c4b61f10b10
Sergio Junior <sergiaraujo15@gmail.com>
2023-04-20 16:39:26 -0300

    added dynamic sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://tools-css.vercel.app",
+  siteUrl: process.env.NEXT_PUBLIC_URL || "https://tools-css.vercel.app",
   generateRobotsTxt: true,
   changefreq: "weekly",
+  exclude: ["/blog/blog-sitemap.xml"],
   robotsTxtOptions: {
     additionalSitemaps: [process.env.NEXT_PUBLIC_URL + "blog/blog-sitemap.xml"],
   },
