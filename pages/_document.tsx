3a980e6bf7f4a41d9f0698af3fe481c9141a9722
Sérgio Júnior <sergiaraujo15@gmail.com>
2023-09-08 14:24:50 -0300

    Disabled Analytics and AdSense in dev environment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,35 +10,41 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta
-          name="google-site-verification"
-          content="AqCTozOdxdev-fEuiuWVG5DrgFKrUb3b_BIH5nbyYE8"
-        />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-G0MQ749VYG"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-
-      gtag("config", "G-G0MQ749VYG");`,
-          }}
-        />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2683859460671203"
-          crossOrigin="anonymous"
-        />
+        <GoogleAnalyticsAndAdSense />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
+  );
+}
+
+function GoogleAnalyticsAndAdSense() {
+  const isProductionEnvironment = process.env.NODE_ENV === "production";
+  if (!isProductionEnvironment) return <></>;
+
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="AqCTozOdxdev-fEuiuWVG5DrgFKrUb3b_BIH5nbyYE8"
+      />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-G0MQ749VYG"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-G0MQ749VYG");`,
+        }}
+      />
+    </>
   );
 }
