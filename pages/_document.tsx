c050486d2a0e68d8b92a6033fe559dcd4326f243
Sérgio Júnior <sergiaraujo15@gmail.com>
2023-11-02 22:06:38 -0300

    Added more icons with different sizes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,10 @@ import { inter, roboto_mono } from "./_app";
 
 export default function Document() {
   return (
-    <Html
-      lang="en-us"
-      className={`bg-zinc-800 ${inter.variable} ${roboto_mono.variable}`}
-    >
+    <Html lang="en-us" className={`bg-zinc-800 ${inter.variable} ${roboto_mono.variable}`}>
       <Head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <GoogleAnalyticsAndAdSense />
       </Head>
@@ -26,14 +24,8 @@ function GoogleAnalyticsAndAdSense() {
 
   return (
     <>
-      <meta
-        name="google-site-verification"
-        content="AqCTozOdxdev-fEuiuWVG5DrgFKrUb3b_BIH5nbyYE8"
-      />
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-G0MQ749VYG"
-      />
+      <meta name="google-site-verification" content="AqCTozOdxdev-fEuiuWVG5DrgFKrUb3b_BIH5nbyYE8" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-G0MQ749VYG" />
       <script
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
@@ -44,6 +36,11 @@ function GoogleAnalyticsAndAdSense() {
 
       gtag("config", "G-G0MQ749VYG");`,
         }}
+      />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2683859460671203"
+        crossOrigin="anonymous"
       />
     </>
   );
